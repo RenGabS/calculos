cf6e1a0ec3f35cec085009594402496c7c8c5569
RenGabS <129408159+RenGabS@users.noreply.github.com>
2023-09-28 12:03:43 -0300

    Add files via upload

---
+++ calculos.js
@@ -0,0 +1,14 @@
+/*console.log("Está Funcionando");
+
+let anoDenascimento = window.prompt ("Em que ano você nasceu ?");
+console.log (anoDenascimento);
+alert ("Quem Nasceu em" + anoDenascimento + " Vai fazer XXXX anos em 2023");
+*/
+
+//-------------------------------------------------//
+//calculadora de nota usando a função prompt
+//perguntar as notas e mostrar o resultado via função alert
+
+let notal = window.prompt ("Nota do primeiro trimestre: ");
+
+console.log(nota1);23
